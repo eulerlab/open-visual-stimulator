d500ff1002721bfb5fdb1a43ff736164ed6c945f
andre <andremaia.chagas@gmail.com>
2018-12-07 13:44:58 +0000

    new led driver circuit

--- resources_for_zebra_fish/lcr_emulator/lcr_emulator.ino
+++ resources_for_zebra_fish/lcr_emulator/lcr_emulator.ino
@@ -1,32 +1,3 @@
- /*
-  Blink without Delay
-
-  Turns on and off a light emitting diode (LED) connected to a digital pin,
-  without using the delay() function. This means that other code can run at the
-  same time without being interrupted by the LED code.
-
-  The circuit:
-  - Use the onboard LED.
-  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
-    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
-    is set to the correct LED pin independent of which board is used.
-    If you want to know what pin the on-board LED is connected to on your
-    Arduino model, check the Technical Specs of your board at:
-    https://www.arduino.cc/en/Main/Products
-
-  created 2005
-  by David A. Mellis
-  modified 8 Feb 2010
-  by Paul Stoffregen
-  modified 11 Nov 2013
-  by Scott Fitzgerald
-  modified 9 Jan 2017
-  by Arturo Guadalupi
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
-*/
 
 // constants won't change. Used here to set a pin number:
 const int pin12 =  12;// the number of the LED pin
@@ -68,10 +39,10 @@ void loop() {
   
   if (millis10 - millis1 <= 400) {
     // save the last time you blinked the LED
-    digitalWrite(pin10,HIGH);
+    digitalWrite(pin10,LOW);
   }
   if (millis10-millis1>400){
-    digitalWrite(pin10,LOW);
+    digitalWrite(pin10,HIGH);
   }
     if (millis10-millis1>=2000){
       millis1=micros();
@@ -101,4 +72,3 @@ void loop() {
   }  
 }
     
-
